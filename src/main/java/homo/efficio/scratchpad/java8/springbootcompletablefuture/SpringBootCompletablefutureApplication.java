62df556f7118ee0cca1c05b0f289f39a60c01849
Homo Efficio <homo.efficio@gmail.com>
2018-03-01 00:29:25 +0900

    Initial Commit

---
+++ src/main/java/homo/efficio/scratchpad/java8/springbootcompletablefuture/SpringBootCompletablefutureApplication.java
@@ -0,0 +1,12 @@
+package homo.efficio.scratchpad.java8.springbootcompletablefuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCompletablefutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCompletablefutureApplication.class, args);
+	}
+}
---
+++ src/test/java/homo/efficio/scratchpad/java8/springbootcompletablefuture/SpringBootCompletablefutureApplicationTests.java
@@ -0,0 +1,16 @@
+package homo.efficio.scratchpad.java8.springbootcompletablefuture;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootCompletablefutureApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
